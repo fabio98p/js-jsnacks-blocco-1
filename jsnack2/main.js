639bd787b786677d9f8f53066320c929425370d6
fabio <parisifabio98@gmail.com>
2021-03-03 14:27:47 +0100

    jsnacks blocco1 es.2 completato

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,1 +1,13 @@
 console.log("funzia");
+
+//chiedo all'utente di inserire 2 parole
+var word1 = prompt("insert one word", "gino")
+var word2 = prompt("insert one more word", "gianni")
+
+//creo un if che capisca quale parola è piu lunga o piu corta e le scrivo in sequenza
+if(word1.length > word2.length){
+	console.log(word2, word1);
+}
+else{
+	console.log(word1, word2);
+}
