d8ba0279542a77eab81900635e5e3029bf870590
fabio <parisifabio98@gmail.com>
2021-03-03 16:32:17 +0100

    jsnack blocco 1 es 9 completato

--- jsnack9/main.js
+++ jsnack9/main.js
@@ -1,1 +1,14 @@
 console.log("funzia");
+
+//inizializzo le variabili del caso
+var number = prompt("inserisci un numero", "10")
+var media = 0
+var somma = 0
+//faccio la somma
+for (let i = 1; i <= number; i++) {
+	somma += i	
+}
+//calcola la media
+media = somma/number
+//scrive i risultati
+console.log(somma, media);
