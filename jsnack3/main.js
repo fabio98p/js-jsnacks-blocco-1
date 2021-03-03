f2ebac77e6bd1d60700f0b9c332e05d9bb37d014
fabio <parisifabio98@gmail.com>
2021-03-03 14:37:02 +0100

    jsnacks blocco 1 es.3 completato

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,1 +1,11 @@
 console.log("funzia");
+
+//inizializzo una variabile "somma"
+var somma = 0;
+//creo un for che mi chieda 10 volte di inserire un numero
+for(var i=0; i < 10; i++){
+	//sommo ogni numero inserito nella variabile somma non prima di aver modificato la stringa in un numero
+	somma += parseInt(prompt("insert a number", "1"));
+}
+//scrivo il risultato
+console.log(somma);
