2db42257faa5d46554c487a299a8f0241f64493c
fabio <parisifabio98@gmail.com>
2021-03-03 15:55:43 +0100

    jsnack blocco 1 es7 completato

--- jsnack7/main.js
+++ jsnack7/main.js
@@ -1,1 +1,10 @@
 console.log("funzia");
+
+//avvio un array dove conservare le potenze
+var array = []
+//metto un for con limite massimo 1000 e che moltiplichi ogni volta per 2
+for (var i = 1; i < 1000; i = i*2) {
+	array.push(i)
+}
+//stampo l'array
+console.log(array);
