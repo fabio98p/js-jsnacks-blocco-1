95775301a24bd96d103fe82324ace54330e98179
fabio <parisifabio98@gmail.com>
2021-03-03 15:17:08 +0100

    jsnack vlocco 1 es 5 e 6 completati

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -1,1 +1,17 @@
 console.log("funzia");
+
+//creo un array vuoto
+var array = [];
+var number = 0
+
+//creo un for con 6 cicli
+for (var i = 0; i < 6; i++) {
+	//chiedo al utente di scrivere un numero *6 cicli
+	number = prompt("inserisci un numero", "1");
+	//se il numero dispari allora entra nell'if e scrive il numero
+	if(number%2){
+		array.push(number);
+	}
+}
+//stampo l'array
+console.log(array);
--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,1 +1,13 @@
 console.log("funzia");
+
+//inserisci di quanti numeri vuoi fare il cubo
+var nNumber = prompt("inserisci di quanti numeri vuoi fare il cubo", "10");
+
+var number = 1;
+var array = [];
+for (let i = 0; i < nNumber; i++) {
+	array.push(number*number*number);
+	number +=1;
+}
+
+console.log(array);
