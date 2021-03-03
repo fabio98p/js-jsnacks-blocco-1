97e5ab6a0dcdb8feac8c16efa7c136d5edf71844
fabio <parisifabio98@gmail.com>
2021-03-03 14:50:29 +0100

    jsnack blocco 1 es4 completato

--- jsnack4/main.js
+++ jsnack4/main.js
@@ -1,1 +1,25 @@
 console.log("funzia");
+
+//inizializzo un array con una lista adi nomi
+var list = ["gino", "pippo", "tizio", "caio", "sempronio"]
+//chiedo al ospite il suo nome
+var name = prompt("insert yout name", "gino")
+//inizializzo l'observer che osservera e capira quando il nome verra trovato
+var observer = false;
+
+//creo un for che controlli tutti i nomi della lista
+for (let i = 0; i < list.length; i++) {
+	//controllo nome per nome con un if
+	if(list[i] == name){
+		observer = true;
+		i = list.length;
+	}
+}
+
+//controllo lo stato di observer per capire se il nome è stato trovato
+if(observer){
+	console.log("l'invitato puo entrare alla festa");
+}
+else{
+	console.log("l'invitato non puo entrare alla festa");
+}
