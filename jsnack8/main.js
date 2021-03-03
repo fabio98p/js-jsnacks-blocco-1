11abee582647d00ea030fe4fd5ded668b183f1be
fabio <parisifabio98@gmail.com>
2021-03-03 16:18:53 +0100

    jsnack blocco 1 es 8 completato

--- jsnack8/main.js
+++ jsnack8/main.js
@@ -1,1 +1,14 @@
 console.log("funzia");
+
+//chiedo un numero di 4 cifre e un arrey dove verra scomposta la stringa di numeri
+var number = prompt("inserisci un numero di 4 cifre", "1234");
+var array = number.split("");
+var somma = 0;
+
+//inizializzo il for che fa tanti cicli quanto è lunga la stringa
+for (let i = 0; i < array.length; i++) {
+	//sommo a somma tutti i singoli valori di array
+	somma += parseInt(array[i]);
+}
+//visualizzo il risultato di somma
+console.log(somma);
